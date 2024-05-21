5d157965e5a561c349e4c907d06b4da1fef2bc4a
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-21 15:34:46 +0100

    fix in bribe

--- sourceCode/AASMA - Prison Break/Assets/Scripts/AI/Agents/Prisoner.cs
+++ sourceCode/AASMA - Prison Break/Assets/Scripts/AI/Agents/Prisoner.cs
@@ -45,7 +45,7 @@ public class Prisoner : Agent
         if (arrested) return;
         List<Action> actions = GetAvailableActions();
         if (canBribe)
-            foreach (Guard guard in guardInfo.Keys.ToList().Where(guard => guardInfo[guard] == currentRoom)) {
+            foreach (Guard guard in guardInfo.Keys.ToList().Where(guard => guardInfo[guard] == targetRoom)) {
                 if (guard.prisonersToIgnore.Contains(this)) continue;
                 Room room = roomWaypoints[guardInfo[guard] - 1].transform.parent.GetComponent<Room>();
                 actions.Add(new Bribe(this, guard, room));
